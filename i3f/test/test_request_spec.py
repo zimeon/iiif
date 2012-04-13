2f50703dac98ae813eb38051219132515592ef01
Simeon Warner <simeon.warner@cornell.edu>
2012-04-13 22:49:34 +0200

    First pass at 0.1 to 0.2 changes

--- i3f/test/test_request_spec.py
+++ i3f/test/test_request_spec.py
@@ -2,15 +2,13 @@
 
 """Test encoding and decoding of the request URLs in the i3f spec
 
-For http://library.stanford.edu/iiif/image-api/ v0.1 dated "9 March 2012"
-
-Run as ./i3f_urltest_spec.py
+For http://library.stanford.edu/iiif/image-api/ v0.2 dated "13 April 2012"
 
 This test includes only test cases for the table in section 7. See
 i3f_urltest.py for more examples that test other cases and alternative 
 forms which should still be decoded correctly.
 
-Simeon Warner - 2012-03-23
+Simeon Warner - 2012-03-23, 2012-04-13
 """
 import unittest
 
@@ -20,43 +18,47 @@ from i3f.request import I3fRequest
 # name : [ {args}, 'canonical_url', 'alternate_form1', ... ]
 # 
 data  = {
-    '01_params': [
-        {'identifier':'id1', 'region':'full', 'size':'100,100', 'rotation':'0', 'color':'color' },
-        'id1/full/100,100/0/color'],
+    '01_identity': [
+        {'identifier':'id1', 'region':'full', 'size':'full', 'rotation':'0', 'quality':'native' },
+        'id1/full/full/0/native'],
     '02_params': [
-        {'identifier':'id1', 'region':'full', 'size':'100,100', 'rotation':'0', 'color':'color', 'format':'jpg' },
-        'id1/full/100,100/0/color.jpg'],
+        {'identifier':'id1', 'region':'0,10,100,200', 'size':'pct:50', 'rotation':'90', 'quality':'native', 'format':'png' },
+        'id1/0,10,100,200/pct:50/90/native.png'],
     '03_params': [
-        {'identifier':'bb157hs6068', 'region':'full', 'size':'pct:100', 'rotation':'270', 'color':'grey', 'format':'jpg'},
-        'bb157hs6068/full/pct:100/270/grey.jpg'],
-# Named profile
-    '04_profile': [
-        {'identifier':'id1', 'profile':'thumb', },
-        'id1/thumb'],
-    '05_profile': [
-        {'identifier':'id1', 'profile':'thumb', 'format':'png' },
-        'id1/thumb.png'],
-# ARKs from http://tools.ietf.org/html/draft-kunze-ark-00
-# ark:sneezy.dopey.com/12025/654xz321
-# ark:/12025/654xz321
-    '06_profile': [
-        {'identifier':'ark:/12025/654xz321', 'profile':'mobile'},
-        'ark:%2F12025%2F654xz321/mobile'],
-# URNs from http://tools.ietf.org/html/rfc2141
-# urn:foo:a123,456
-    '07_profile': [
-        {'identifier':'urn:foo:a123,456', 'profile':'mobile'},
-        'urn:foo:a123,456/mobile'],
-# URNs from http://en.wikipedia.org/wiki/Uniform_resource_name
-# urn:sici:1046-8188(199501)13:1%3C69:FTTHBI%3E2.0.TX;2-4
-# ** note will get double encoding **
-    '08_profile': [
-        {'identifier':'urn:sici:1046-8188(199501)13:1%3C69:FTTHBI%3E2.0.TX;2-4', 'profile':'mobile'},
-        'urn:sici:1046-8188(199501)13:1%253C69:FTTHBI%253E2.0.TX;2-4/mobile'],
-# Extreme silliness
-    '09_profile': [
-        {'identifier':'http://example.com/?54#a', 'profile':'mobile'},
-        'http:%2F%2Fexample.com%2F%3F54%23a/mobile'],
+        {'identifier':'id1', 'region':'pct:10,10,80,80', 'size':'50,', 'rotation':'22.5', 'quality':'color', 'format':'jpg' },
+        'id1/pct:10,10,80,80/50,/22.5/color.jpg'],
+    '04_params': [
+        {'identifier':'bb157hs6068', 'region':'full', 'size':'full', 'rotation':'270', 'quality':'grey', 'format':'jpg'},
+        'bb157hs6068/full/full/270/grey.jpg'],
+    # ARKs from http://tools.ietf.org/html/draft-kunze-ark-00
+    # ark:sneezy.dopey.com/12025/654xz321
+    # ark:/12025/654xz321
+    '05_ark': [
+        {'identifier':'ark:/12025/654xz321', 'region':'full', 'size':'full', 'rotation':'0', 'quality':'native'},
+        'ark:%2F12025%2F654xz321/full/full/0/native'],
+    # URNs from http://tools.ietf.org/html/rfc2141
+    # urn:foo:a123,456
+    '06_urn': [
+        {'identifier':'urn:foo:a123,456', 'region':'full', 'size':'full', 'rotation':'0', 'quality':'native'},
+        'urn:foo:a123,456/full/full/0/native'],
+    # URNs from http://en.wikipedia.org/wiki/Uniform_resource_name
+    # urn:sici:1046-8188(199501)13:1%3C69:FTTHBI%3E2.0.TX;2-4
+    # ** note will get double encoding **
+    '07_urn': [
+        {'identifier':'urn:sici:1046-8188(199501)13:1%3C69:FTTHBI%3E2.0.TX;2-4', 'region':'full', 'size':'full', 'rotation':'0', 'quality':'native'},
+
+        'urn:sici:1046-8188(199501)13:1%253C69:FTTHBI%253E2.0.TX;2-4/full/full/0/native'],
+    # Extreme silliness
+    '08_http': [
+        {'identifier':'http://example.com/?54#a', 'region':'full', 'size':'full', 'rotation':'0', 'quality':'native'},
+        'http:%2F%2Fexample.com%2F%3F54%23a/full/full/0/native'],
+    # Information requests
+    '10_info': [
+        {'identifier':'id1', 'info':True, 'format':'json' },
+        'id1/info.json'],
+    '11_info': [
+        {'identifier':'id1', 'info':True, 'format':'xml' },
+        'id1/info.xml'],
     }
 
 class TestAll(unittest.TestCase):
@@ -78,16 +80,19 @@ class TestAll(unittest.TestCase):
             for turl in data[tname][1:]:
                 i3f = I3fRequest().parse_url(turl)
                 tstr = self.pstr(i3f.__dict__)
-                print tname + "   " + tstr + " -> " + turl
+                print "    <tr>\n      <td>" + tstr + "</td>\n      <td>" + turl + "</td>\n    </tr>"
                 self.assertEqual(tstr,pstr)
         print
 
     def pstr(self,p):
-        str=''
-        for k in ['identifier','region','size','rotation','color','profile','format']:
-            if k in p and p[k]:
-                str += k+'='+p[k]+' '
-        return(str)
+        s=''
+        for k in ['identifier','region','size','rotation','quality','info','format']:
+            if (k in p and p[k]):
+                if (k=='info'):
+                    s += 'image information request, '
+                else:
+                    s += k+'='+str(p[k])+' '
+        return(s)
         
 # If run from command line, do tests
 if __name__ == '__main__':
