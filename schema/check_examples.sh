359bd3a4f07b961a4ea623330ff680736f0f9c0f
Simeon Warner <simeon.warner@cornell.edu>
2012-07-12 11:03:35 -0400

    Add Relax-NG schema and make tests run that too

--- schema/check_examples.sh
+++ schema/check_examples.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 echo "# All good cases should say 'valididates'"
-for xml in *_ex*.xml
-  do xmllint --noout --schema image-api.xsd $xml
+for xml in *_ex*.xml; do
+  echo -n "xsd: "
+  xmllint --noout --schema image-api.xsd $xml
+  echo -n "rng: "
+  xmllint --noout --relaxng image-api.rng $xml
 done
 
 echo "# All bad cases should say 'fails to validate'"
-for xml in *bad*xml
-  do xmllint --noout --schema image-api.xsd $xml  2>&1 | grep -v "Schemas validity error"
+for xml in *bad*xml; do
+  echo -n "xsd: "
+  xmllint --noout --schema image-api.xsd $xml  2>&1 | grep -v "Schemas validity error"
+  echo -n "rng: "
+  xmllint --noout --relaxng image-api.rng $xml  2>&1 | grep -v "Relax-NG validity error"
 done
 
