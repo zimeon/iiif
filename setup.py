b10170ebb5d69bd813313965585426279eee8508
Simeon Warner <simeon.warner@cornell.edu>
2013-05-21 08:11:22 -0400

    Fixup for pypi

--- setup.py
+++ setup.py
@@ -14,13 +14,24 @@ else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE))
 
 setup(
-    name='I3f',
-    version='0.1.0',
+    name='i3f',
+    version=version,
     author='Simeon Warner',
+    author_email='simeon.warner@cornel.edu',
     packages=['i3f'],
     scripts=[],
+    classifiers=["Development Status :: 3 - Alpha",
+                 "Intended Audience :: Developers",
+                 "License :: OSI Approved :: GPLv3",
+                 "Operating System :: OS Independent", #is this true? know Linux & OS X ok
+                 "Programming Language :: Python",
+                 "Programming Language :: Python :: 2.6",
+                 "Programming Language :: Python :: 2.7",
+                 "Topic :: Internet :: WWW/HTTP",
+                 "Topic :: Software Development :: Libraries :: Python Modules",
+                 "Environment :: Web Environment"],
     url='https://github.com/zimeon/i3f',
     license='LICENSE.txt',
-    description='Code to share for I3F',
-    long_description=open('README.txt').read(),
+    description='IIIF Image API library',
+    long_description=open('README').read(),
 )
