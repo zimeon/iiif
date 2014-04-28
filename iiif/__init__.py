0fb04af64b25a12009d677805ba4e92db100f8ff
Simeon Warner <simeon.warner@cornell.edu>
2014-04-28 18:09:47 -0400

    Remove API version because we now support multiple versions

--- iiif/__init__.py
+++ iiif/__init__.py
@@ -1,10 +1,5 @@
 from _version import __version__
 
-# Version of IIIF Image API supported
-__api_major__ = 2
-__api_minor__ = 0
-__api_patch__ = 0
-
 from config import IIIFConfig
 from error import IIIFError
 from info import IIIFInfo
