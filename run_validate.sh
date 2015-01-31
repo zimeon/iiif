9aea2b51ac5439db570e699b188d83cb75276422
Simeon Warner <simeon.warner@cornell.edu>
2015-01-31 09:55:15 -0500

    Ren level 2 test for level 2 (along with grey fix in manipulater), improve verbose output

--- run_validate.sh
+++ run_validate.sh
@@ -10,13 +10,16 @@
 # See http://tldp.org/LDP/abs/html/arithexp.html for bash arithmetic
 
 verbosity='--quiet'
-while getopts ":v:d" opt; do
+show_test_name=false
+while getopts "vd" opt; do
   case $opt in
     v)
       verbosity=''
+      show_test_name=true
       ;;
     d)
       verbosity='--verbose'
+      show_test_name=true
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -30,17 +33,31 @@ done
 
 # Run validations against test server
 errors=0
+if $show_test_name; then
+  echo "Testing PIL manipulator, level 1.1"
+fi
 iiif-validate.py -s localhost:8000 -p 1.1_pil_none -i 67352ccc-d1b0-11e1-89ae-279075081939.png --version=1.1 --level 1 $verbosity
 ((errors+=$?))
-iiif-validate.py -s localhost:8000 -p 2.0_pil_none -i 67352ccc-d1b0-11e1-89ae-279075081939.png --version=2.0 --level 1 $verbosity
+if $show_test_name; then
+  echo "Testing PIL manipulator, level 2.0"
+fi
+iiif-validate.py -s localhost:8000 -p 2.0_pil_none -i 67352ccc-d1b0-11e1-89ae-279075081939.png --version=2.0 --level 2 $verbosity
 ((errors+=$?))
 #iiif-validate.py -s localhost:8000 -p 1.1_netpbm_none -i 67352ccc-d1b0-11e1-89ae-279075081939.png --version=1.1 --level 1 $verbosity
 #((errors+=$?))
+if $show_test_name; then
+  echo "Testing netpbm manipulator, level 2.0"
+fi
 iiif-validate.py -s localhost:8000 -p 2.0_netpbm_none -i 67352ccc-d1b0-11e1-89ae-279075081939.png --version=2.0 --level 1 $verbosity
 ((errors+=$?))
 
 # Kill test server
 kill `cat iiif_testserver.pid`
 
-echo "$0 finished (total of $errors errors)"
+if test $errors -eq 0; then
+  msg="no errors"
+else
+  msg="$errors errors, use -v or -d for details"
+fi
+echo "$0 finished ($msg)"
 exit $errors
