def19766632756d00da4a23ae616cab9b81b712b
Simeon Warner <simeon.warner@cornell.edu>
2015-07-27 17:45:00 -0400

    More tests

--- tests/test_auth_basic.py
+++ tests/test_auth_basic.py
@@ -3,6 +3,8 @@
 See http://flask.pocoo.org/docs/0.10/testing/ for Flask notes
 """
 from flask import Flask,request, make_response, redirect
+from werkzeug.datastructures import Headers
+import base64
 import json
 import re
 import unittest
@@ -53,6 +55,23 @@ class TestAll(unittest.TestCase):
             self.assertEqual( response.headers['Content-type'], 'text/html' )
             html = response.get_data()
             self.assertEqual( html, '' )
+        # add good login params and check OK, window close
+        h = Headers()
+        h.add('Authorization', 'Basic ' + base64.b64encode('userpass:userpass'))
+        with dummy_app.test_request_context('/a_request', headers=h):
+            response = auth.login_handler()
+            self.assertEqual( response.status_code, 200 )
+            html = response.get_data()
+            self.assertTrue( re.search(r'<script>window.close\(\);</script>',html) )
+            set_cookie = response.headers['Set-Cookie']
+            self.assertTrue( re.search(auth.auth_cookie_name+'=valid-http-basic-login', set_cookie) )
+        # add bad login params and check fail
+        h = Headers()
+        h.add('Authorization', 'Basic ' + base64.b64encode('userpass:bad-pass'))
+        with dummy_app.test_request_context('/a_request', headers=h):
+            response = auth.login_handler()
+            self.assertEqual( response.status_code, 401 )
+
  
     def test06_logout_handler(self):
         with dummy_app.test_request_context('/a_request'):
@@ -70,5 +89,29 @@ class TestAll(unittest.TestCase):
             self.assertEqual( response.status_code, 200 )
             self.assertEqual( response.headers['Content-type'], 'application/json' )
             j = json.loads(response.get_data())
+            self.assertEqual( j['error_description'], "No login details received" )
+            self.assertEqual( j['error'], "client_unauthorized" )
+        # add Authorization header, check we get token
+        h = Headers()
+        h.add('Authorization', 'Basic ' + base64.b64encode('userpass:userpass'))
+        with dummy_app.test_request_context('/a_request', headers=h):
+            auth = IIIFAuthBasic()
+            response = auth.access_token_handler()
+            self.assertEqual( response.status_code, 200 )
+            self.assertEqual( response.headers['Content-type'], 'application/json' )
+            j = json.loads(response.get_data())
+            self.assertEqual( j['access_token'], "secret_token_here" ) #FIXME
+            self.assertEqual( j['token_type'], "Bearer" )
+            self.assertEqual( j['expires_in'], 3600 )
+        # add callback but no Authorization header
+        with dummy_app.test_request_context('/a_request?callback=CB'):
+            auth = IIIFAuthBasic()
+            response = auth.access_token_handler()
+            self.assertEqual( response.status_code, 200 )
+            self.assertEqual( response.headers['Content-type'], 'application/javascript' )
+            # strip JavaScript wrapper and then check JSON
+            js = response.get_data()
+            self.assertTrue( re.match('CB\(.*\);',js) )
+            j = json.loads(js.lstrip('CB(').rstrip(');'))
             self.assertEqual( j['error_description'], "No login details received" )
             self.assertEqual( j['error'], "client_unauthorized" )
