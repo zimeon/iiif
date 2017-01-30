f3a3ea56a43ca7e7f50393f9f943db594a1a153f
Simeon Warner <simeon.warner@cornell.edu>
2017-01-29 22:55:17 -0500

    Classifiers and changelog

--- setup.py
+++ setup.py
@@ -65,9 +65,9 @@ setup(
                  "Programming Language :: Python",
                  "Programming Language :: Python :: 2.6",
                  "Programming Language :: Python :: 2.7",
-                 "Programming Language :: Python :: 3.3",
                  "Programming Language :: Python :: 3.4",
                  "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6",
                  "Topic :: Internet :: WWW/HTTP",
                  "Topic :: Multimedia :: Graphics :: Graphics Conversion",
                  "Topic :: Software Development :: "
