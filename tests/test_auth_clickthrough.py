1625b1c340d57a62d8b28da4b4c99491c9573d6c
Simeon Warner <simeon.warner@cornell.edu>
2017-02-15 20:18:14 -0500

    Simple test

---
+++ tests/test_auth_clickthrough.py
@@ -0,0 +1,55 @@
+"""Test code for iiif.auth_clickthrough.
+
+See http://flask.pocoo.org/docs/0.10/testing/ for Flask notes.
+"""
+from flask import Flask, request, make_response, redirect
+from werkzeug.datastructures import Headers
+import base64
+import json
+import re
+import unittest
+
+from iiif.auth_clickthrough import IIIFAuthClickthrough
+
+dummy_app = Flask('dummy')
+
+
+class TestAll(unittest.TestCase):
+    """Tests."""
+
+    def setUp(self):
+        """Set up dummy app."""
+        self.app = dummy_app.test_client()
+
+    def tearDown(self):
+        """No op."""
+        pass
+
+    def test01_init(self):
+        """Test initialization."""
+        auth = IIIFAuthClickthrough()
+        self.assertEqual(auth.auth_pattern, 'clickthrough')
+        self.assertTrue(re.match(r'\d+_', auth.cookie_prefix))
+        auth = IIIFAuthClickthrough(cookie_prefix='abc')
+        self.assertEqual(auth.cookie_prefix, 'abc')
+
+    def test02_login_service_description(self):
+        """Test login_service_description."""
+        auth = IIIFAuthClickthrough()
+        desc = auth.login_service_description()
+        self.assertEqual(auth.confirm_label, desc['confirmLabel'])
+
+    def test03_login_handler(self):
+        """Test login_handler."""
+        with dummy_app.test_request_context('/a_request'):
+            auth = IIIFAuthClickthrough()
+            response = auth.login_handler()
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.headers['Content-type'], 'text/html')
+            html = response.get_data().decode('utf-8')  # data is bytes in python3
+            self.assertTrue(
+                re.search(r'<script>window.close\(\);</script>', html))
+            set_cookie = response.headers['Set-Cookie']
+            self.assertTrue(
+                re.search(auth.account_cookie_name + '=clickthrough-ok;',
+                          set_cookie))
